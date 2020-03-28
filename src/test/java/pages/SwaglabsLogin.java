9cce2751da6a290a136240d183f9ec28a05b452e
VITTAL B <vittalbeerappagari17@gmail.com>
2020-03-28 09:33:35 +0530

    added comments to swaglabslogin

--- src/test/java/pages/SwaglabsLogin.java
+++ src/test/java/pages/SwaglabsLogin.java
@@ -9,20 +9,23 @@ import swaglabs.WrapperClass;
 
 public class SwaglabsLogin {
 	
+	// opens the application in browser
 	public void launchApplication() {
 		WrapperClass.openBrowser();
 	}
 	
+	// enters the login details
 	public void enterLoginDetails(String userName, String password) {
 		WrapperClass.driver.findElement(By.xpath("//input[@id='user-name']")).sendKeys(userName);
 		WrapperClass.driver.findElement(By.xpath("//input[@id='password']")).sendKeys(password);
 	}
 	
+	// clicks on submit button
 	public void clickSubmitButton() {
 		WrapperClass.driver.findElement(By.xpath("//input[@type='submit']")).click();
 	}
 	
-	
+	// closes the application and browser
 	public void closeApplication() {
 		WrapperClass.closeBrowser();
 	}
